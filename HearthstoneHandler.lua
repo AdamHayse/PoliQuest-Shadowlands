513884e2b1c5b02213dd08a0cbf1412b051c742f
Adam <ahayse501@gmail.com>
2020-10-18 01:28:00 -0400

    Start implementing refactor

---
+++ HearthstoneHandler.lua
@@ -0,0 +1,48 @@
+  
+local _, addonTable = ...
+
+local C_GossipInfo, UnitLevel, UnitName = C_GossipInfo, UnitLevel, UnitName
+
+do -- Set hearthstone
+    local onGossipShow_setHearth = function()
+        if innWhitelist[GossipFrameNpcNameText:GetText()] then
+            local gossipOptions = C_GossipInfo.GetOptions()
+            local numOptions = C_GossipInfo.GetNumOptions()
+            for i=1, numOptions do
+                if gossipOptions[i]["type"] == "binder" then
+                    C_GossipInfo.SelectOption(i)
+                    StaticPopup1Button1:Click("LeftButton")
+                end
+            end
+        end
+    end
+
+    local onConfirmBinder = function()
+        if UnitLevel("player") < 60 then
+            local targetName = UnitName("target")
+            if GossipFrameNpcNameText:GetText() and innWhitelist[GossipFrameNpcNameText:GetText()]
+            or targetName and innWhitelist[targetName] then
+                StaticPopup1Button1:Click()
+            end
+        end
+    end
+
+    local onEvent = function(self, event, ...)
+        if event == "GOSSIP_SHOW" then
+            onGossipShow_setHearth()
+        elseif event == "CONFIRM_BINDER" then
+            onConfirmBinder()
+        elseif event == "GOSSIP_CLOSED" then
+            onConfirmBinder()
+        end
+    end
+    
+    local hearthHandler = CreateFrame("Frame", "PoliHearthHandler")
+    hearthHandler:SetScript("OnEvent", onEvent)
+    hearthHandler.Events = {
+       "GOSSIP_SHOW",
+       "CONFIRM_BINDER",
+       "GOSSIP_CLOSED"
+    }
+    addonTable.hearthHandler = hearthHandler
+end
